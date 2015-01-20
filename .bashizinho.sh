0ddf41ae5ede5206445e94e45cf3a621b79564a0
Lucas Castejon <lucascastejon@gmail.com>
2015-01-19 23:23:35 -0200

    Added source to code alias and exports

--- .bashizinho.sh
+++ .bashizinho.sh
@@ -1,6 +1,8 @@
 unset color_prompt force_color_prompt
 
 export PATH="$PATH:$HOME/.rvm/bin" # Add RVM to PATH for scripting
-source /home/lucas/.rvm/scripts/rvm
+source /$HOME/.rvm/scripts/rvm
 
 source /$HOME/projetos/bashrc/.bashizinho/functions
+source /$HOME/projetos/bashrc/.bashizinho/alias
+source /$HOME/projetos/bashrc/.bashizinho/exports
