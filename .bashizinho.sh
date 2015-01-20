a57c6add5931a45c74839f9175edc33f0a32417e
Lucas Castejon <lucascastejon@gmail.com>
2015-01-19 23:11:06 -0200

    Altering functions, and moving to folder and file only functions

---
+++ .bashizinho.sh
@@ -0,0 +1,6 @@
+unset color_prompt force_color_prompt
+
+export PATH="$PATH:$HOME/.rvm/bin" # Add RVM to PATH for scripting
+source /home/lucas/.rvm/scripts/rvm
+
+source /$HOME/projetos/bashrc/.bashizinho/functions
